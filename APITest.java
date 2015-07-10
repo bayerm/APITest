51ce342e7eb6a06539c79b186d165a662f4d8201
Morgan <mbayer.school@gmail.com>
2015-07-10 14:00:28 -0700

    Initial Test

---
+++ APITest.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+class APITest{
+	public static void main(String[] args) throws IOException{
+		URL url = new URL("http://download.finance.yahoo.com/d/quotes.csv?s=BA");
+		URLConnection con = url.openConnection();
+		InputStream is = con.getInputStream();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		
+		String line = null;
+		
+		while ((line = br.readLine()) != null){
+			System.out.println(line);
+		}
+	}
+}
